057bd792b781d5dc2ee9f82516cbc885bceb9102
cesarbassani <cesarbassani.dev@gmail.com>
2020-01-29 16:35:49 -0400

    SearchBox

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/pages/home.page.dart';
 import 'package:shopping/pages/signup.page.dart';
 
 class LoginPage extends StatelessWidget {
@@ -125,9 +126,16 @@ class LoginPage extends StatelessWidget {
                               color: Colors.white,
                             ),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => HomePage(),
+                              ),
+                            );
+                          },
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
