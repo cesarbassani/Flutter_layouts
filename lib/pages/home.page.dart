04e2df73b7ade8cffeca94df5dd35fe769ccecb5
cesarbassani <cesarbassani.dev@gmail.com>
2020-01-30 09:49:30 -0400

    SliverAppBar

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,61 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/pages/product.page.dart';
-import 'package:shopping/pages/signup.page.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(15),
-        color: Color(0xFFF5F5F5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(
-              height: 60,
-            ),
-            search(),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Categories",
-              style: TextStyle(
-                fontSize: 30,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          color: Color(0xFFF5F5F5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(
+                height: 60,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 90,
-              child: categoryList(),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Best Selling",
-                  style: Theme.of(context).textTheme.headline,
+              search(),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Categories",
+                style: TextStyle(
+                  fontSize: 30,
                 ),
-                FlatButton(
-                  child: Text("See All"),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 350,
-              child: productList(context),
-            )
-          ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 90,
+                child: categoryList(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Best Selling",
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                  FlatButton(
+                    child: Text("See All"),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 350,
+                child: productList(context),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/product.page.dart
+++ lib/pages/product.page.dart
@@ -4,13 +4,27 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Text("Detalhes do produto"),
-        ),
-      ),
-    );
+        body: NestedScrollView(
+      body: Container(),
+      headerSliverBuilder: (BuildContext context, bool innerBoxInScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.white.withOpacity(0),
+            elevation: 0.0,
+            expandedHeight: 500.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              background: Image.asset(
+                "assets/product-10.png",
+                width: double.infinity,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+        ];
+      },
+    ));
   }
 }
