b2aecea4a47150becf18d6efe5c704a75165cd31
cesarbassani <cesarbassani.dev@gmail.com>
2020-02-04 09:35:53 -0400

    ProductList e ProdutItem Widget

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shopping/pages/product.page.dart';
 import 'package:shopping/widgets/category/category-list.widget.dart';
+
+import 'package:shopping/widgets/product/product-list.widget.dart';
 import 'package:shopping/widgets/search-box.widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -55,90 +56,14 @@ class HomePage extends StatelessWidget {
               ),
               Container(
                 height: 350,
-                child: productList(context),
-              )
+                child: ProductList(
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-}
-
-Widget productList(BuildContext context) {
-  return Container(
-    child: ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        productItem(context),
-        productItem(context),
-        productItem(context),
-        productItem(context),
-      ],
-    ),
-  );
-  ;
-}
-
-Widget productItem(BuildContext context) {
-  return Container(
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.all(5),
-      width: 170,
-      color: Colors.black12,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ProductPage(),
-                ),
-              );
-            },
-            child: Image.asset(
-              "assets/product-1.png",
-              width: 170,
-              height: 170,
-              fit: BoxFit.cover,
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            height: 60,
-            child: Text(
-              "Título do produto",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            "Marca",
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            "\$ 200",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF00C569),
-            ),
-          ),
-        ],
-      ));
 }
