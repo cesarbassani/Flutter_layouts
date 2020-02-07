dd7fabbe9338e39369b6ed4d8dfb13e1189be1ae
cesarbassani <cesarbassani.dev@gmail.com>
2020-02-07 16:58:52 -0400

    Animation Icon

--- lib/widgets/search-box.widget.dart
+++ lib/widgets/search-box.widget.dart
@@ -8,6 +8,7 @@ class SearchBox extends StatefulWidget {
 class _SearchBoxState extends State<SearchBox>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
+  bool menuOpened = false;
 
   @override
   void initState() {
@@ -28,42 +29,22 @@ class _SearchBoxState extends State<SearchBox>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      padding: EdgeInsets.only(
-        left: 20,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.1),
-        borderRadius: BorderRadius.all(
-          Radius.circular(128),
-        ),
-      ),
-      child: Row(
-        children: <Widget>[
-          Icon(Icons.search),
-          Container(
-            width: 300,
-            padding: EdgeInsets.only(left: 10),
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                labelText: "Search...",
-                labelStyle: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 16,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text("Search"),
+        FlatButton(
+          onPressed: () {
+            !menuOpened ? _controller.forward() : _controller.reverse();
+            menuOpened = !menuOpened;
+          },
+          child: AnimatedIcon(
+            icon: AnimatedIcons.menu_close,
+            progress: _controller,
+            semanticLabel: 'Open Menu',
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
